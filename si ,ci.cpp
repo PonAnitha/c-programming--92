f7a04a81c0873321010e9d6f4697b54a1402e531
PonAnitha <143417421+PonAnitha@users.noreply.github.com>
2023-09-24 15:29:43 +0530

    Update si ,ci.cpp

--- si ,ci.cpp
+++ si ,ci.cpp
@@ -4,11 +4,12 @@
 int main()
 {
 	int p,n,r;
-	float si,ci;
+	float si,ci,amt;
 	printf("Enter the values of principle, number of years,rate of interest:");
 	scanf("%d%d%d",&p,&n,&r);
 	si=(p*n*r)/100;
-	ci=p* pow((1-(r/100)),n);
+	amt=p* pow((1+(r/100)),n);
+	ci=p-amt;
 	printf("The simple interest is=%f\n",si);
 	printf("The compound interest is =%f\n",ci);
 	return 0;
